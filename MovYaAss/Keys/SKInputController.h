e7aef413aa85cfb77a18aa0851264267bc18418e
Dragos Tudorache <hedphug@gmail.com>
2014-02-21 01:41:15 +0200

    add input controller

---
+++ MovYaAss/Keys/SKInputController.h
@@ -0,0 +1,10 @@
+//
+// Created by Dragos on 2/21/14.
+// Copyright (c) 2014 Dragos Tudorache. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SKInputController : NSObject
+@end
