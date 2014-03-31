93e248f6006090e07cf660aeff1f6004321acb38
Dragos Tudorache <hedphug@gmail.com>
2014-04-01 00:43:25 +0300

    add proper tile detection interface and doc blocks

--- MovYaAss/SKTileMatrix.h
+++ MovYaAss/SKTileMatrix.h
@@ -11,12 +11,26 @@
 // the invisible frame for the tiles wrapper rect
 @property (nonatomic, assign) CGRect wrapperFrame;
 
-// initializers
-- (id) initWithRows:(int)rows columns:(int)columns;
-- (id) initWithRows:(int)rows columns:(int)columns visibleFrame:(CGRect)frame;
 
-// public interface
-//- (id) getTileAtPoint:(CGPoint)point;
+- (id) initWithRows:(int)rows
+            columns:(int)columns;
+
+- (id) initWithRows:(int)rows
+            columns:(int)columns
+       visibleFrame:(CGRect)frame;
+
+
+// finds the tile where this rectangle is located
+- (NSRect) tileForRectangle:(NSRect)rect;
+
+// finds the tile where this rectangle is located testing its containment for a particular axis
+// It tests the containment(intersection) over the x or y axis, deciding between two tiles on the
+// x or two tiles on the y(if the current tile is not fully contained, it selects the upper/right-er
+// tile, meaning that that's the tile containing the rect)
+- (NSRect) tileForRectangle:(NSRect)rect
+             relativeToAxis:(NSString *)axis;
+
+// @todo remove testFindTileForRect: temporary method
 - (void)testFindTileForRect:(NSRect)frame;
 
 @end
